5b05bd4353476413f650f89338cab3581efe017b
CarnotaurusGit <animakeriaev@gmail.com>
2025-09-24 11:02:59 -0600

    settings.py

--- app/settings.py
+++ app/settings.py
@@ -21,7 +21,7 @@ import os
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'os.environment.get('SECRET_KEY,default='sid123456')'
+SECRET_KEY = os.environ.get('SECRET_KEY,default='sid123456')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
